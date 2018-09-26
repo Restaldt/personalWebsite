5827af814a6135d16c6aed2343239696827726c0
Blake Arender <blakearender@gmail.com>
2018-09-26 17:29:41 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function homeFunction(){
+
+	//alert("Home Button Clicked");
+
+	document.getElementById("mainBlock").innerHTML = "<h3>	About Me: </h3>"
+
+}
+
+function ipfsFunction(){
+	//alert("IPFS button clicked");
+	document.getElementById("mainBlock").innerHTML = "";
+
+}
